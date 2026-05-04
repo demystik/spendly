a2f6a7059b2a6f2a0b134f23574663bf734ad987
demysti <demystiktech@gmail.com>
2026-05-04 14:00:00 +0100

    data adk

--- lib/themes/app_spacing.dart
+++ lib/themes/app_spacing.dart
@@ -12,7 +12,6 @@ abstract class AppNewSpacing {
   static const double sm  = 8;
   static const double md  = 16;
   static const double lg  = 24;
-  static const double xl  = 32;
 }
 
 abstract class AppRadius {
