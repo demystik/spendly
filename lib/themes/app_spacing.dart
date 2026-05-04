6a89e2d718cb31a22aa394e7390807a4242680fe
demysti <demystiktech@gmail.com>
2026-05-04 14:00:00 +0100

    xl deleted

--- lib/themes/app_spacing.dart
+++ lib/themes/app_spacing.dart
@@ -11,7 +11,6 @@ abstract class AppNewSpacing {
   static const double xs  = 4;
   static const double sm  = 8;
   static const double md  = 16;
-  static const double lg  = 24;
 }
 
 abstract class AppRadius {
