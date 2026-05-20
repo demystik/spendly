c6e028370e006ece24b2e7a36dc3e27cdb6d3cf5
demysti <demystiktech@gmail.com>
2026-05-20 08:01:18 +0100

    working  on budget screen

--- lib/screens/budget_screen.dart
+++ lib/screens/budget_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class BudgetScreen extends StatelessWidget {
   const BudgetScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Budget Plan"),
+        actions: [
+        Icon(LucideIcons.circlePlus)
+      ],),
+      );
   }
 }
