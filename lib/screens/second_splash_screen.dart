557d80ed954a24ee5357290a6759aeed549ce798
demysti <demystiktech@gmail.com>
2026-05-20 07:44:18 +0100

    second screen added

--- lib/screens/second_splash_screen.dart
+++ lib/screens/second_splash_screen.dart
@@ -1,10 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:spendly/themes/app_spacing.dart';
+import 'package:spendly/themes/app_text_styles.dart';
+import 'package:spendly/widgets/app_button.dart';
+import 'package:spendly/widgets/app_chip.dart';
 
-class SecondSplashScreen extends StatelessWidget {
+class SecondSplashScreen extends StatefulWidget {
   const SecondSplashScreen({super.key});
 
   @override
+  State<SecondSplashScreen> createState() => _SecondSplashScreenState();
+}
+
+class _SecondSplashScreenState extends State<SecondSplashScreen> with SingleTickerProviderStateMixin {
+  late Animation<double> _buttonSize;
+  late AnimationController ctrl;
+    late Animation<Offset> slider;
+
+  @override
+  void initState() {
+    super.initState();
+
+    ctrl = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 700),
+      );
+
+    _buttonSize = Tween<double>(begin: 10, end: 30,).animate(
+      CurvedAnimation(parent: ctrl, curve: Curves.easeIn),
+    );
+
+    slider = Tween<Offset>(begin: Offset(1.0, 0.0), end: Offset.zero).animate(CurvedAnimation(parent: ctrl, curve: Curves.bounceIn),);
+    
+    ctrl.forward();
+    
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenSize = MediaQuery.of(context).size;
+    final screenColorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(AppSpacing.md),
+          child: AnimatedBuilder(
+            animation: ctrl,
+                    builder: (context, child) => Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: AlignmentGeometry.centerRight,
+                  child: AppChip(
+                    onTap: () => context.pop(),
+                    label: "Skip"),
+                ),
+                SizedBox(height: AppSpacing.lg),
+                SizedBox(
+                  width: screenSize.width * 0.6,
+                  height: screenSize.width * 0.6,
+                  child: SvgPicture.asset("assets/animations/online-banking.svg"),
+                ),
+                Spacer(),
+                SlideTransition(
+                  position: slider,
+                  child: RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "Manage Budgets",
+                          style: AppTextStyles.displayLarge,
+                        ),
+                        TextSpan(
+                          text: "\nSmartly",
+                          style: AppTextStyles.displayLarge.copyWith(
+                            color: screenColorScheme.primary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: AppSpacing.md),
+                Opacity(
+                  opacity: 0.7,
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    "Set custo limits for categories and get real-time alerts before you overspend.",
+                    style: AppTextStyles.titleMedium,
+                    maxLines: 4,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+            
+                Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 5,
+                  children: [
+                    SplashButton(
+                      screenColorScheme: screenColorScheme,
+                      main: false,
+                    ),
+                    SplashButton(
+                        screenColorScheme: screenColorScheme,
+                        main: true, size: _buttonSize.value,
+                      ),
+                    
+                    SplashButton(
+                      screenColorScheme: screenColorScheme,
+                      main: false,
+                    ),
+                  ],
+                ),
+            
+                SizedBox(height: AppSpacing.md),
+                AppButton(
+                  label: "Continue >",
+                  onPressed: () {
+                    context.push("/third_splash_screen");
+                  },
+                ),
+            
+                SizedBox(height: AppSpacing.md),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SplashButton extends StatelessWidget {
+  const SplashButton({
+    super.key,
+    required this.screenColorScheme,
+    required this.main,
+    this.size,
+  });
+
+  final ColorScheme screenColorScheme;
+  final bool main;
+  final double? size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 8,
+      width: main ? size : 8,
+      decoration: BoxDecoration(
+        color: main
+            ? screenColorScheme.primary
+            : screenColorScheme.surfaceContainerHighest,
+        borderRadius: BorderRadius.circular(AppRadius.md),
+      ),
+    );
   }
 }
