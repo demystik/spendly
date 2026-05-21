f513c453d45033d101f2fd249118da5e56d36f8f
demysti <demystiktech@gmail.com>
2026-05-21 17:04:20 +0100

    anaylitics

--- lib/screens/analytics_screen.dart
+++ lib/screens/analytics_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:spendly/themes/app_spacing.dart';
+import 'package:spendly/themes/app_text_styles.dart';
 import 'package:spendly/widgets/app_card.dart';
 
 class AnalyticsScreen extends StatelessWidget {
@@ -8,8 +9,9 @@ class AnalyticsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ColorScheme appColorScheme = Theme.of(context).colorScheme;
     return Scaffold(appBar: AppBar(
-      title: Text("Analytics"),,
+      title: Text("Analytics"),
       actions: [
         Icon(LucideIcons.share2),
         SizedBox(width: AppSpacing.md,),
@@ -17,13 +19,47 @@ class AnalyticsScreen extends StatelessWidget {
         SizedBox(width: AppSpacing.md,),
       ],
     ),
-    body: SafeArea(child: ListView(children: [
-      AppCard(
-        child: Column(children: [
-        Text("SPENT"),
-        Text("\$1,824"),
-        Text("12% this month"),
-      ],),),
+    body: SafeArea(child: ListView(
+      padding: EdgeInsets.all(AppSpacing.md),
+      children: [
+      Row(
+        children: [
+          Container(
+            width: ,
+            padding: EdgeInsets.all(AppSpacing.md),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(AppRadius.md),
+            border: Border.all(color: appColorScheme.surfaceContainerHighest),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+            Text("SPENT"),
+            Text("\$1,824", style: AppTextStyles.titleLarge.copyWith(height: 1.5),),
+            Text("12% this month"),
+          ],),),
+          AppCard(
+            boxshadow: [BoxShadow()],
+            border: Border.all(color: appColorScheme.surfaceContainerHighest),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+            Text("AVG/DAY"),
+            Text("\$61.40", style: AppTextStyles.titleLarge.copyWith(height: 1.5),),
+            Text("5% calculated"),
+          ],),),
+          AppCard(
+            boxshadow: [BoxShadow()],
+            border: Border.all(color: appColorScheme.surfaceContainerHighest),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+            Text("BUDGET"),
+            Text("74%", style: AppTextStyles.titleLarge.copyWith(height: 1.5),),
+            Text("Used up"),
+          ],),),
+        ],
+      ),
     ],),),
     );
   }
