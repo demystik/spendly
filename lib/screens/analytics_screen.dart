5c0344f88b5d77e4d12c2337d6ebf3c8ddead741
demysti <demystiktech@gmail.com>
2026-05-21 16:48:22 +0100

    budget screen adjusted

--- lib/screens/analytics_screen.dart
+++ lib/screens/analytics_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:spendly/themes/app_spacing.dart';
 
 class AnalyticsScreen extends StatelessWidget {
   const AnalyticsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: AppBar(
+      title: Text("Analytics"),
+      actions: [
+        Icon(LucideIcons.share2),
+        SizedBox(width: AppSpacing.md,),
+
+        Icon(LucideIcons.download),
+        SizedBox(width: AppSpacing.md,),
+      ],
+    ),);
   }
 }
