5bead2c4a7a8dc264a953e7dc9aa43f580bdbff0
demysti <demystiktech@gmail.com>
2026-05-24 16:52:05 +0100

    category amount updated

--- lib/providers/category_provider.dart
+++ lib/providers/category_provider.dart
@@ -2,6 +2,16 @@ import 'package:flutter/widgets.dart';
 import 'package:spendly/models/category_model.dart';
 
 class CategoryProvider with ChangeNotifier{
+
+  final List<Category> _budgetCategories = List.from(categoryList);
+
+  List<Category> get budgetCategories => _budgetCategories;
+
+  void updateBudgetCategories(double budgetAmount, Category category){
+    budgetCategories.firstWhere((cat) => cat.id == category.id).amountToSpend = budgetAmount;
+    notifyListeners();
+  }
+
   Category? _selectedCategory;
 
   Category? get selectedCategory => _selectedCategory;
