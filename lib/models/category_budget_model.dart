4dd64362a88db450cedff58f85823a3363746cec
demysti <demystiktech@gmail.com>
2026-05-24 17:03:14 +0100

    category model adjusted

---
+++ lib/models/category_budget_model.dart
@@ -0,0 +1,13 @@
+class CategoryBudget {
+  String month;
+  String year;
+  double budgetAmount;
+  String categoryId;
+
+  CategoryBudget({
+    required this.month,
+    required this.year,
+    required this.budgetAmount,
+    required this.categoryId,
+  });
+}
--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -6,14 +6,12 @@ class Category {
   String name;
   Color color;
   IconData icon;
-  double amountToSpend;
 
   Category({
     required this.id,
     required this.name,
     required this.color,
     required this.icon,
-    required this.amountToSpend,
   });
 }
 
@@ -24,48 +22,41 @@ List<Category> categoryList = [
     name: "Transport",
     color: Colors.blue,
     icon: LucideIcons.car,
-    amountToSpend: 0.0,
   ),
   Category(
     id: 3452342.342,
     name: "Food & Drinks",
     color: Colors.green,
     icon: LucideIcons.coffee,
-    amountToSpend: 0.0,
   ),
   Category(
     id: 321242342.342,
     name: "Shopping",
     color: Colors.indigo,
     icon: LucideIcons.shoppingBag,
-    amountToSpend: 0.0,
   ),
   Category(
     id: 009142342.342,
     name: "Groceries",
     color: Colors.lightBlue,
     icon: LucideIcons.utensils,
-    amountToSpend: 0.0,
   ),
   Category(
     id: 30042342.342,
     name: "Bills",
     color: Colors.brown,
     icon: LucideIcons.wallet,
-    amountToSpend: 0.0,
   ),
   Category(
     id: 300442,
     name: "Health",
     color: Colors.amber,
     icon: LucideIcons.heartPulse,
-    amountToSpend: 0.0,
   ),
   Category(
     id: 300232442,
     name: "Entertainment",
     color: Colors.redAccent,
     icon: LucideIcons.gamepad2,
-    amountToSpend: 0.0,
   ),
 ];
