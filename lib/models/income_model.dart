ead93d5cfc410c3d6284685135baa51144fc6389
demysti <demystiktech@gmail.com>
2026-05-30 10:29:50 +0100

    monthly budget hive updated

--- lib/models/income_model.dart
+++ lib/models/income_model.dart
@@ -1,6 +1,17 @@
-class IncomeModel {
-  final double amount;
-  final DateTime updatedAt;
+// class IncomeModel {
+//   final double amount;
+//   final DateTime updatedAt;
+
+import 'package:hive_flutter/adapters.dart';
+
+@HiveType(typeId: 2)
+class IncomeModel extends HiveObject{
+
+  @HiveField(0)
+  double amount;
+
+  @HiveField(1)
+  DateTime updatedAt;
 
   IncomeModel({
     required this.amount,
--- lib/models/monthly_budget_model.dart
+++ lib/models/monthly_budget_model.dart
@@ -1,7 +1,22 @@
-class MonthlyBudgetModel {
+import 'package:hive_flutter/adapters.dart';
+// class MonthlyBudgetModel {
+//   double monthlyBudgetAmount;
+//   DateTime dateUpdated;
+//   String month;
+//   String year;
+
+@HiveType(typeId: 1)
+class MonthlyBudgetModel extends HiveObject {
+  @HiveField(0)
   double monthlyBudgetAmount;
+
+  @HiveField(1)
   DateTime dateUpdated;
+
+  @HiveField(2)
   String month;
+
+  @HiveField(3)
   String year;
 
   MonthlyBudgetModel({
