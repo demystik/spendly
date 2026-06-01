86c7b4407e92ef1116bd39bbefea44c35842ec51
demysti <demystiktech@gmail.com>
2026-06-01 09:31:14 +0100

    login button added

---
+++ lib/shared/login_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spendly/providers/auth_provider.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      onPressed: () {
+        context.read<AuthProvider>().login();
+      },
+      icon: Icon(Icons.login),
+      label: Text("Continue with Google"),
+    );
+  }
+}
