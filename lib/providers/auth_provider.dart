c714eb24fb55ea82301b46b845c072fb79eff1e9
demysti <demystiktech@gmail.com>
2026-06-01 09:30:43 +0100

    provider added

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthProvider
+    with ChangeNotifier {
+  User? _user;
+
+  AuthProvider() {
+    FirebaseAuth.instance
+        .authStateChanges()
+        .listen((user) {
+      _user = user;
+      notifyListeners();
+    });
+  }
+
+  User? get user => _user;
+
+  bool get isLoggedIn =>
+      _user != null;
+}
