7b4c63c8aab9af4cb69356b8316fb4f13812081e
demysti <demystiktech@gmail.com>
2026-06-05 12:04:14 +0100

    redirection for debugging

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -32,9 +32,6 @@ final GoRouter appRouter = GoRouter(
   initialLocation: '/',
 
   redirect: (context, state) {
-  print("Redirect running");
-  print("Current location: ${state.uri.path}");
-  print("Current status: ${authProvider.status}");
     final loc = state.uri.path;
     final status = authProvider.status;
 
